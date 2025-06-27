ad51d76d4bb6b42ec1eaa5ce39905f73cf345034
sathishtcz <msathish.tcz@gmail.com>
2025-06-27 10:19:55 +0530

    Vercell

--- src/component/Organizingpage/Organizingpage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Organizingcommittee from '../../shared/components/Organizingcommittee/Organizingcommittee'
-
-export default function Organizingpage() {
-  return (
-    <div><Organizingcommittee /></div>
-  )
-}
